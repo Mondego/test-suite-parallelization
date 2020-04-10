59ddbfd967211506b6834918e0aad789eab56dca
marufzaber <mzaber@uci.edu>
2020-04-10 14:40:44 -0700

    Spoon added. Spoon will be used to estimate runtime of a method

---
+++ src/main/java/com/cin/pag/loadestimation/ASTBuilder.java
@@ -0,0 +1,15 @@
+package com.cin.pag.loadestimation;
+
+import spoon.reflect.code.CtInvocation;
+
+/**
+ * This class build the AST with the help of Spoon.
+ * @author demigorgan
+ *
+ */
+
+public class ASTBuilder {
+	public void process(CtInvocation element) {
+       
+    }
+}
