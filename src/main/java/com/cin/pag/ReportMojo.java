8aac84035e05b6e7dbf07a17ab0bf020ae775028
marufzaber <mzaber@uci.edu>
2020-04-14 22:27:08 -0700

    dependency to Maven Surefire and Surefire Plugin added

---
+++ src/main/java/com/cin/pag/ReportMojo.java
@@ -0,0 +1,12 @@
+package com.cin.pag;
+
+import org.apache.maven.plugins.surefire.report.SurefireReportMojo;
+
+/**
+ * This mojo writes the time of each test methods in an CSV file.
+ * @author demigorgan
+ *
+ */
+public class ReportMojo extends SurefireReportMojo {
+
+}
