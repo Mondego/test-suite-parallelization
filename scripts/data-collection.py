0240e6e1a93e2a0f36aaca1bbdac5523611aacbb
vanessa <vanessa.klotzman.17@my.csun.edu>
2020-10-11 16:54:41 -0700

    clone java projects and added conditions for more than 100 commits and stars

--- scripts/data-collection.py
+++ scripts/data-collection.py
@@ -7,19 +7,10 @@ import requests
 import pandas as pd
 import datetime
 from github import Github
-
-dirName='java_projects'
-
-try:
-    os.mkdir(dirName)
-    os.chdir(dirName)
-
-except FileExistsError:
-    print(dirName+"exists")
-
-
+#user your username 
 username = "vanessailana"
-ACCESS_TOKEN = 'put your token here'
+#generate a token 
+ACCESS_TOKEN = ''
 g=Github();
 
 #we need a token for the usual api limit
@@ -31,28 +22,29 @@ if rate.remaining == 0:
 else:
     print(f'You have {rate.remaining}/{rate.limit} API calls remaining')
  
-user = g.get_user(username)
-
-for repo in user.get_repos():
-     #print_repo(repo)
-     #print(repo.clone_url)
-    #os.system("git clone {}".format(repo["git_url"]))
-   #print(dir(repo))
-
     repo_name=[];
     stars=[];
     language=[];
     created_at=[];
+    git_url=[];
+    commit_count=[];
 #search java projects on github  pick 100 
 #for i, repo in enumerate(g.search_repositories("topic:maven")): 
-    for i,repo in  enumerate(g.search_repositories("topic:maven")):
+print("test")
+for i,repo in  enumerate(g.search_repositories("topic:maven")):
         #amount of public repos 
-        print("="*100)
+        print("="*1000)
+        if i==100:
+            break
         temp_data = {}
         temp_data["name"] = repo.name
         temp_data["stars"] = repo.stargazers_count
         temp_data['language']=repo.language
-        temp_data['created_at']=repo.created_at
+        temp_data['commit_count']=repo.get_commits().totalCount
+        temp_data['git_url']=repo.git_url
+        #os.system("git clone {}".format(repo.git_url))
+        
+
 
 
         #print(dir(repo)
@@ -62,8 +54,10 @@ for repo in user.get_repos():
 
         language.append(temp_data['language']);
 
-        created_at.append(temp_data['created_at']);
-      
+        git_url.append(temp_data['git_url']);
+
+        commit_count.append(temp_data['commit_count']);
+
         java_projects=pd.DataFrame()
 
         java_projects['name']=repo_name;
@@ -72,20 +66,34 @@ for repo in user.get_repos():
 
         java_projects['language']=language;
 
-        java_projects.loc[java_projects['stars'] >=100]
+        java_projects['git_url']=git_url
+
+        java_projects['commits']=commit_count
+
+        java_projects.loc[java_projects['stars'] >=50]
+
+        java_projects.loc[java_projects['commits'] >=100]
+
         java_projects.loc[java_projects['language']=='java']
 
-        print(java_projects.head(3))
-       
+        #uncomment this line when you aread to download projects
+        dirName='cloned_projects'
+        os.mkdir(dirName)
+        os.chdir(dirName)
+       #os.system("git clone {}".format(java_projects["git_url"]))
+
+  
         
+
+        print(java_projects.head(3))
         java_projects.to_csv('project_metadata.csv') 
 
-
-
-    #metadata about projects in json 
-    #f = open("java"+"/metadata.json", 'w')
-    #f.write(json.dumps(metadata))
-    #f.close()
+ 
+ 
+    
+    
+    
+    
 
 
 
